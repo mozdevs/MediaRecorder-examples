0b4472761c12146e8d0e019fc84ab1920e56305c
sole <sole@mozilla.com>
2016-01-06 17:35:09 +0000

    Add example for getting a stream out of media devices

---
+++ stream-from-mediadevices.js
@@ -0,0 +1,26 @@
+// This example uses MediaDevices.getUserMedia to get an audio and video stream,
+// and then display it in a video element
+//
+// The relevant functions in use are:
+//
+// navigator.mediaDevices.getUserMedia -> to get live audio + video stream from webcam
+// URL.createObjectURL -> to create a URL for the stream, which we can use as src for the video
+
+
+window.onload = function () {
+
+  // request video and audio stream from the user's webcam
+  navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: true
+  })
+  .then(function (stream) {
+    var video = document.createElement('video');
+    var main = document.querySelector('main');
+    main.appendChild(video);
+    video.src = URL.createObjectURL(stream);
+    video.play();
+  });
+  
+};
+
