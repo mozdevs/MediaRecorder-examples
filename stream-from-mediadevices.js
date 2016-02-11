c2c58ac48c263b4cc7ead665ddb1be6c8054902b
sole <sole@mozilla.com>
2016-02-11 14:40:14 +0000

    Fix typo, remove ribbon

--- stream-from-mediadevices.js
+++ stream-from-mediadevices.js
@@ -1,11 +1,10 @@
 // This example uses MediaDevices.getUserMedia to get an audio and video stream,
-// and then display it in a video element
+// and then displays it in a video element
 //
 // The relevant functions in use are:
 //
 // navigator.mediaDevices.getUserMedia -> to get live audio + video stream from webcam
 // URL.createObjectURL -> to create a URL for the stream, which we can use as src for the video
-
 
 window.onload = function () {
 
