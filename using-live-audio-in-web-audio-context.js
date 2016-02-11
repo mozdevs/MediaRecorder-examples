b40dabeee5791d39b6c4797aff2a1bf0a451215b
sole <sole@mozilla.com>
2016-02-11 15:08:06 +0000

    Renaming for clarity

--- using-live-audio-in-web-audio-context.js
+++ using-live-audio-in-web-audio-context.js
@@ -1,10 +1,11 @@
-// This example uses MediaDevices.getUserMedia to get an audio stream,
+// This example uses MediaDevices.getUserMedia to get a live audio stream,
 // and then bring it into a Web Audio context using a MediaStreamSourceNode
 //
 // The relevant functions in use are:
 //
 // navigator.mediaDevices.getUserMedia -> to get live audio stream from webcam
-// AudioContext.createMediaStreamSource -> to create a node that takes a MediaStream as input and outputs its audio data
+// AudioContext.createMediaStreamSource -> to create a node that takes a MediaStream
+// as input and works as source of sound inside the audio graph
 
 window.onload = function () {
 
